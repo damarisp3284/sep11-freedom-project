5867dda90a624500460818cbc5657facf03fa9a0
Damaris <damarisp19@nycstudents.net>
2025-11-08 22:26:26 +0000

    Sat, Nov 8, 2025, 5:26 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -1,45 +1,27 @@
-class Example extends Phaser.Scene
-    {
-        preload ()
-        {
-            this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
+import Phaser from 'phaser';
 
-            this.load.image('sky', 'assets/skies/space3.png');
-            this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-            this.load.image('red', 'assets/particles/red.png');
-        }
-
-        create ()
-        {
-            this.add.image(400, 300, 'sky');
-
-            const particles = this.add.particles(0, 0, 'red', {
-                speed: 100,
-                scale: { start: 1, end: 0 },
-                blendMode: 'ADD'
-            });
-
-            const logo = this.physics.add.image(400, 100, 'logo');
-
-            logo.setVelocity(100, 200);
-            logo.setBounce(1, 1);
-            logo.setCollideWorldBounds(true);
-
-            particles.startFollow(logo);
-        }
+// Phaser configuration
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-    const config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 600,
-        scene: Example,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 200 }
-            }
-        }
-    };
+const game = new Phaser.Game(config);
 
-    const game = new Phaser.Game(config);
+function preload() {
+    this.load.image('logo', 'https://labs.phaser.io/assets/sprites/phaser3-logo.png');
+}
+
+function create() {
+    const logo = this.add.image(400, 300, 'logo');
+}
+
+function update() {
+    // Game loop logic
+}
