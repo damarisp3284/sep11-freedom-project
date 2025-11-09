fb8487de084635079da94de308d2ce6c2b35f3ed
Damaris <damarisp19@nycstudents.net>
2025-11-09 01:49:31 +0000

    Sat, Nov 8, 2025, 8:49 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -13,7 +13,7 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-       this.load.image('sky', 'assets/sky.png');
+    this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
@@ -22,6 +22,7 @@ function preload ()
         { frameWidth: 32, frameHeight: 48 }
     );
 }
+
 
 function create ()
 {
@@ -32,5 +33,39 @@ function create ()
 function update ()
 {
 }
+
+var platforms;
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+}
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
 
 
