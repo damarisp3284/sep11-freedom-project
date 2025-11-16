9cf2081441dbc3211b41a1b094721c6243d774a1
Damaris <damarisp19@nycstudents.net>
2025-11-16 22:39:39 +0000

    Sun, Nov 16, 2025, 5:39 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -20,6 +20,9 @@ var player;
 var stars;
 var platforms;
 var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
 
 var game = new Phaser.Game(config);
 
@@ -29,15 +32,14 @@ function preload ()
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
+    this.load.spritesheet('dude', 'assets/dude.png',
+     { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
+    
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
