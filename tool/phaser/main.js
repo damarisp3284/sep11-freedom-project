d17bc69bebc549f236ab9b0b2326b17230031029
Damaris <damarisp19@nycstudents.net>
2025-11-16 22:04:35 +0000

    Sun, Nov 16, 2025, 5:04 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -36,31 +36,6 @@ function create ()
     this.add.image(400, 300, 'sky');
     this.add.image(400, 300, 'star');
 
-    player = this.physics.add.sprite(100, 450, 'dude');
-
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
 }
 
 var platforms;
@@ -77,8 +52,6 @@ function create ()
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 }
-
-
 
 
 function update ()
