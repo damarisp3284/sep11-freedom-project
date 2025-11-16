6e1741811da9614820f454fc36a57917be746526
Damaris <damarisp19@nycstudents.net>
2025-11-16 22:27:39 +0000

    Sun, Nov 16, 2025, 5:27 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -104,10 +104,24 @@ var scoreText;
 
 scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
+bombs = this.physics.add.group();
 
+this.physics.add.collider(bombs, platforms);
 
+this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
 
+}
 
 function update ()
 {
@@ -133,6 +147,42 @@ else
 if (cursors.up.isDown && player.body.touching.down)
 {
     player.setVelocityY(-330);
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+    }
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+}
+
 }
 }
 
