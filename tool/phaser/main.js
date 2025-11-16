f5b1b84d3f6597f41d07862469ebefd09d8525fd
Damaris <damarisp19@nycstudents.net>
2025-11-16 21:29:55 +0000

    Sun, Nov 16, 2025, 4:29 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -70,8 +70,37 @@ function preload ()
             repeat: -1
         });
 
-        this.physics.add.collider(player, platforms);
+        cursors = this.input.keyboard.createCursorKeys();
 
+        this.physics.add.collider(player, platforms);
+    }
+
+    function update()
+    {
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        rameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+     key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
 
  }
