8942913ae0762089e26b41d267cb25cce7e522c9
Damaris <damarisp19@nycstudents.net>
2025-11-16 22:19:30 +0000

    Sun, Nov 16, 2025, 5:19 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -71,8 +71,38 @@ this.anims.create({
     repeat: -1
 });
 
-this.physics.add.collider(player, platforms);
+ cursors = this.input.keyboard.createCursorKeys();
 
+ stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+});
+
+stars.children.iterate(function (child) {
+
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+});
+
+
+this.physics.add.collider(player, platforms);
+this.physics.add.collider(stars, platforms);
+
+this.physics.add.overlap(player, stars, collectStar, null, this);
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+}
+
+var score = 0;
+var scoreText;
+
+scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
 
 
