dd19ea0f81ada552da1dc48abf49cd300250219d
Damaris <damarisp19@nycstudents.net>
2025-11-16 21:48:55 +0000

    Sun, Nov 16, 2025, 4:48 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -98,36 +98,10 @@ function preload ()
         this physics.add.collider(stars, platform);
 
         this physics.add.overlap(player, starts, collectStar, null, this);
+
+        this.physics.add.collider(player, bombs, htibomb, null, this);
     }
-    player = this.physics.add.sprite(100, 450, 'dude');
-
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        rameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-     key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    cursors = this.input.keyboard.creatorCursorKeys();
-
-    this.physics.add.collider(player, platforms);
-    }
-
+   
      function update ()
     {
         if (cursors.left.isDown)
