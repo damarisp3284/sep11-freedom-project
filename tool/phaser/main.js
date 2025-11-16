9479f9e3466a18b9ee3bbbcad55ff4e50537ad04
Damaris <damarisp19@nycstudents.net>
2025-11-16 21:17:19 +0000

    Sun, Nov 16, 2025, 4:17 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -16,6 +16,9 @@ var config = {
     }
 };
 
+var player;
+var platforms;
+
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -38,23 +41,30 @@ function create ()
      platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     this.add.image(400, 300, 'star');
-}
+}player = this.physics.add.sprite(100, 450, 'dude');
 
-// Platform creation
-var platforms;
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
-    platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
 
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
 
-
-}
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
 
 
 
