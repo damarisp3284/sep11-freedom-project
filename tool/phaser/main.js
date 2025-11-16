0e1a42ffc23477fc5d5994810f209d3bc2f2e25b
Damaris <damarisp19@nycstudents.net>
2025-11-16 21:43:37 +0000

    Sun, Nov 16, 2025, 4:43 PM -05:00

--- tool/phaser/main.js
+++ tool/phaser/main.js
@@ -146,6 +146,9 @@ function preload ()
         function collectStar (player, star)
         {
             star.disableBody(true, true);
+
+            score += 10;
+            scoreText.setText('Score: ' + score);
         }
 
 
